480f2a512304692ce6e71442d6382108bf5c3a8d
Ryan Attard <ryanattard@gmail.com>
2022-02-14 10:16:21 +0100

    initial commit with login (Excl keys)

---
+++ SWD63A2022/SWD63A2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SWD63A2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
